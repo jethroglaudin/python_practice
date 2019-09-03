b9a92f7e3a20546370c31a65e2330066255b7acf
Jethro Glaudin <jethro_glaudin@my.uri.edu>
2019-09-02 20:59:40 -0400

    input function

---
+++ input.py
@@ -0,0 +1,13 @@
+# name = input('What is your name? ')
+# print('Hi ' + name)
+
+# input is a function that built in to python
+# this input function will print message onto the terminal and wait for an input from the user
+
+
+# Practice
+
+user_name = input("What is your name? ")
+color = input("What is your favorite color? ")
+
+print(user_name + " likes " + color)
