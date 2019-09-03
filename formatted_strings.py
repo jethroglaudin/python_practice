c51945bb8627246ab63f1e56ab10ee92f03f77b5
Jethro Glaudin <jethro_glaudin@my.uri.edu>
2019-09-02 21:35:58 -0400

    formatted strings

---
+++ formatted_strings.py
@@ -0,0 +1,8 @@
+first = 'Jethro'
+last = 'Glaudin'
+
+message = first + ' [' + last + '] is a coder'
+msg = f'{first} [{last}] is a coder'
+# formatted string - prefix string with f
+# the curly braces act as placeholders
+print(msg)
