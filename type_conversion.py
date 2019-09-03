9b2b63ec2e0b9f46115cd05e62822eb14e1a1817
Jethro Glaudin <jethro_glaudin@my.uri.edu>
2019-09-02 21:18:14 -0400

    converting str to ints

---
+++ type_conversion.py
@@ -0,0 +1,17 @@
+# birth_year = input("Birth year: ")
+# print(type(birth_year))
+# age = 2019 - int(birth_year)
+# print(type(age))
+# print(age)
+
+# int() function converts a string into an interger
+# float() function converts a string into a float
+# bool() function converts string to a boolean value
+# type() shows us what type is the value that's being passed through
+
+# Excersise
+
+user_weight = input("Weight (*lbs): ")
+kg = int(user_weight) * 0.45
+
+print(kg, "kg")
