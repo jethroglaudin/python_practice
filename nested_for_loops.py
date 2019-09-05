a69482c18db2c2cfa913c5df1409cb1b4938d73e
Jethro Glaudin <jethro_glaudin@my.uri.edu>
2019-09-05 18:23:48 -0400

    nested for loop practice

---
+++ nested_for_loops.py
@@ -0,0 +1,11 @@
+# for x in range(4):
+#    for y in range(3):
+#        print(f'({x}, {y})')
+
+numbers = [5, 2, 5, 2, 2]
+
+for j in numbers:
+    output = ''
+    for k in range(j):
+        output += 'x'
+    print(out
