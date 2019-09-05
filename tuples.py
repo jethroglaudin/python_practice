176f491dfce7c489ee0d88469b3b1011be3b29b2
Jethro Glaudin <jethro_glaudin@my.uri.edu>
2019-09-05 19:29:47 -0400

    practice with tuples

---
+++ tuples.py
@@ -0,0 +1,8 @@
+# Tuples are like lists but we cannot modify them
+# tuples are immutable
+
+# tubles are good to use if you don't want something modified
+
+numbers = (1, 2 , 3)
+numbers[0] = 10
+print(numbers[0])
