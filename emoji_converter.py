1d9a4859cb1529c72bbd50444a0c3f0311f48f99
Jethro Glaudin <jethro_glaudin@my.uri.edu>
2019-09-15 21:28:41 -0400

    emoji converer practicing dictionaries with python

---
+++ emoji_converter.py
@@ -0,0 +1,16 @@
+ message = input(">")
+words = message.split(" ")
+
+emojis = {
+    ":)": "😀",
+    ":(": "☹️",
+    ":'(": "😢",
+    ":|": "😐",
+    ":p": "😜"
+
+}
+output = ""
+for word in words:
+    output += emojis.get(word, word) + " "
+
+print(output)
