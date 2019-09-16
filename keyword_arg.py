fc4be17e646cdd9860779f4093d17ba46fe2c9cf
Jethro Glaudin <jethro_glaudin@my.uri.edu>
2019-09-15 21:55:15 -0400

    keyword arguments

---
+++ keyword_arg.py
@@ -0,0 +1,11 @@
+# def is a reserved keyword in python and it's short for define.
+
+def greet_user(first_name, last_name):
+    print(f'Hi, {first_name} {last_name}!')
+    print('Welcome aboard')
+
+
+print("Start")
+greet_user(last_name="Glaudin", first_name="Glaudin")
+
+print("Finish")
